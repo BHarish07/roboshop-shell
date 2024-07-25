8d25409050f6e89a03b6e3711bd6f39e12c4a45a
BHarish07 <harishbalike1995@gmail.com>
2024-07-25 12:47:15 +0530

    shell-scripting

--- mysql.sh
+++ mysql.sh
@@ -27,3 +27,16 @@ then
 else
   echo "You are super user.."
 fi
+
+dnf install mysql-server -y &>> $LOG_FILE
+VALIDATE $? "Installing MySQL server"
+
+systemctl enable mysqld &>> $LOG_FILE
+VALIDATE $? " Enabling "MySQL server
+
+systemctl start mysqld &>> $LOG_FILE
+VALIDATE $? "Starting MySQL server"
+
+mysql_secure_installation --set-root-pass RoboShop@1 &>> $LOG_FILE
+VALIDATE $? "Setting root Password"
+

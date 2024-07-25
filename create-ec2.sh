9ae4bcb57e226c9c9f7687fa6a116b2f8a9c6a08
BHarish07 <harishbalike1995@gmail.com>
2024-07-25 11:20:01 +0530

    shell-scripting-ec2

--- create-ec2.sh
+++ create-ec2.sh
@@ -2,15 +2,16 @@
 
 instances=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "web")
 domain_name="harishbalike.online"
-hosted_zone_id="Z0591062122N9AA3RRJGE"
-region="ap-south-1"
-sg_id="sg-0c2f413dbe8baeba6"
-subnet_id="subnet-0edef79e101f8b225"
-image_id="ami-0ad21ae1d0696ad58"
+hosted_zone_id="Z013862017DKOONOMKNDC"
+region="us-east-1"
+sg_id="sg-08492f9229eff00e6"
+subnet_id="subnet-039c946b04bc14df1"
+image_id="ami-041e2ea9402c46c32"
 
 for name in ${instances[@]} ;do
 
   if [ $name == "mysql" ] || [ $name == "shipping" ] 
+  
   then
     instance_type="t3.medium"
   else
@@ -20,20 +21,24 @@ for name in ${instances[@]} ;do
 
   echo "Creating instances for $name with instance type $instance_type"
 
-  instance_id=$(aws ec2 run-instances --image-id $image_id  --instance-type $instance_type --key-name mumbai --security-group-ids $sg_id --subnet-id $subnet_id  --query "Instances[0].InstanceId" --output text)
+  instance_id=$(aws ec2 run-instances --image-id $image_id  --instance-type $instance_type --security-group-ids $sg_id --subnet-id $subnet_id  --query "Instances[0].InstanceId" --output text)
 
   echo "Instance created for: $name"
 
   aws ec2 create-tags --resources $instance_id --tags Key=Name,Value=$name
   
   if [ $name == "web" ] 
-  then 
+
+  then
+   
   public_ip=$(aws --region $region ec2 describe-instances  --query 'Reservations[0].Instances[0].[PublicIpAddress]' --output text)
   ip_to_use=$public_ip
 
   else
+
   private_ip=$(aws --region $region ec2 describe-instances  --query 'Reservations[0].Instances[0].[PrivateIpAddress]' --output text)
   ip_to_use=$private_ip
+
   fi
 
   aws route53 change-resource-record-sets --hosted-zone-id $hosted_zone_id  --change-batch '

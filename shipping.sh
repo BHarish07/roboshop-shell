7632600f757b769e435760947ccc203d9d62f023
BHarish07 <harishbalike1995@gmail.com>
2024-07-26 23:57:58 +0530

    sg

--- shipping.sh
+++ shipping.sh
@@ -3,7 +3,7 @@
 USERID=$(id -u)
 TIMESTAMP=$(date +%F-%H-%M-%S)
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
-LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
+LOG_FILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
@@ -29,59 +29,62 @@ else
 fi
 
 
-dnf install maven -y &>> $LOGFILE
+dnf install maven -y &>> $LOG_FILE
 VALIDATE $? "Installing Maven"
 
-id roboshop &>> $LOGFILE
+id roboshop &>> $LOG_FILE
 if [ $? -ne 0 ]
 then
-    useradd roboshop &>> $LOGFILE
+    useradd roboshop &>> $LOG_FILE
     VALIDATE $? "Adding roboshop user"
 else
     echo -e "roboshop user already exist...$Y SKIPPING $N"
 fi
 
-rm -rf /app &>> $LOGFILE
+rm -rf /app &>> $LOG_FILE
 VALIDATE $? "clean up existing directory"
 
-mkdir -p /app &>> $LOGFILE
+mkdir -p /app &>> $LOG_FILE
 VALIDATE $? "Creating app directory"
 
-curl -L -o /tmp/shipping.zip https://roboshop-builds.s3.amazonaws.com/shipping.zip &>> $LOGFILE
+curl -L -o /tmp/shipping.zip https://roboshop-builds.s3.amazonaws.com/shipping.zip &>> $LOG_FILE
 VALIDATE $? "Downloading shipping application"
 
-cd /app  &>> $LOGFILE
+cd /app  &>> $LOG_FILE
 VALIDATE $? "Moving to app directory"
 
-unzip /tmp/shipping.zip &>> $LOGFILE
+unzip /tmp/shipping.zip &>> $LOG_FILE
 VALIDATE $? "Extracting shipping application"
 
-mvn clean package &>> $LOGFILE
+mvn clean package &>> $LOG_FILE
 VALIDATE $? "Packaging shipping"
 
-mv target/shipping-1.0.jar shipping.jar &>> $LOGFILE
+mv target/shipping-1.0.jar shipping.jar &>> $LOG_FILE
 VALIDATE $? "Renaming the artifact"
 
-cp /home/ec2-user/roboshop-shell/shipping.service /etc/systemd/system/shipping.service &>> $LOGFILE
+cp /home/ec2-user/roboshop-shell/shipping.service /etc/systemd/system/shipping.service &>> $LOG_FILE
 VALIDATE $? "Copying service file"
 
-systemctl daemon-reload &>> $LOGFILE
+systemctl daemon-reload &>> $LOG_FILE
 VALIDATE $? "Daemon reload"
 
-systemctl enable shipping  &>> $LOGFILE
+systemctl enable shipping  &>> $LOG_FILE
 VALIDATE $? "Enabling shipping"
 
-systemctl start shipping &>> $LOGFILE
+systemctl start shipping &>> $LOG_FILE
 VALIDATE $? "Starting shipping"
 
-dnf install mysql -y &>> $LOGFILE
+dnf install mysql -y &>> $LOG_FILE
 VALIDATE $? "Installing MySQL"
-sed -i 's/FLUSH PRIVILEGES/-- FLUSH PRIVILEGES' /app/schema/shipping.sql
-mysql -h $MYSQL_HOST -uroot -pRoboShop@1 -e "use cities" &>> $LOGFILE
+
+sed -i 's/FLUSH PRIVILEGES/-- FLUSH PRIVILEGES/' /app/schema/shipping.sql &>> $LOG_FILE
+
+
+mysql -h $MYSQL_HOST -uroot -pRoboShop@1 -e "use cities" &>> $LOG_FILE
 if [ $? -ne 0 ]
 then
     echo "Schema is ... LOADING"
-    mysql -h $MYSQL_HOST -uroot -pRoboShop@1 < /app/schema/shipping.sql &>> $LOGFILE  #use this cmd and edit the shipping.sql with sudo --delete FLUSH PRIVILEGES;
+    mysql -h $MYSQL_HOST -uroot -pRoboShop@1 < /app/schema/shipping.sql &>> $LOG_FILE  
     VALIDATE $? "Loading schema"
 else
     echo -e "Schema already exists... $Y SKIPPING $N"

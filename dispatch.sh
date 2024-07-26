51c51fa8bf2e9badb480dc42dba39c0776b3eca8
BHarish07 <harishbalike1995@gmail.com>
2024-07-27 00:08:00 +0530

    sg

--- dispatch.sh
+++ dispatch.sh
@@ -28,4 +28,29 @@ else
   echo "You are super user.."
 fi
 
+dnf install golang -y
 
+useradd roboshop
+
+mkdir /app 
+
+curl -L -o /tmp/dispatch.zip https://roboshop-builds.s3.amazonaws.com/dispatch.zip
+
+cd /app
+
+unzip /tmp/dispatch.zip
+
+
+go mod init dispatch
+
+go get 
+
+go build
+
+cp /home/ec2-user/roboshop-shell/dispatch.service /etc/systemd/system/dispatch.service
+
+systemctl daemon-reload
+
+systemctl enable dispatch
+
+systemctl start dispatch
